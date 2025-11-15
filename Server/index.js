f71ae7dea126352bb9452f2ec2bc57ae770f6b97
Ritik Kumar Singh <ritikrajput2550@gmail.com>
2025-11-15 20:28:37 +0530

    latest commit

--- Server/index.js
+++ Server/index.js
@@ -19,28 +19,17 @@ const app = express();
 
 
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://shopping-cart-frontend-1.onrender.com"
-];
+
+
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-        return callback(new Error("Not allowed by CORS"));
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://shopping-cart-frontend-1.onrender.com"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-}));
-
-
-app.options("*", cors());
-
-
-
+}))
 
 
 
