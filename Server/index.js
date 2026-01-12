60978e42e926e3a0decc9f14b93bf7f3c4d9e5f9
Ritik Kumar Singh <ritikrajput2550@gmail.com>
2026-01-12 20:37:18 +0530

    cors setting

--- Server/index.js
+++ Server/index.js
@@ -32,7 +32,8 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://shopping-cart-frontend-1.onrender.com"
+        "https://shopping-cart-frontend-1.onrender.com",
+        "https://shopping-cart-frontend-2.onrender.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
